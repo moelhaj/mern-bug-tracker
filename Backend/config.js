9a31a818bb1ed93d1c3c31966ac763037609f6bb
Mohamed Elhaj <mo.elhaj@outlook.com>
2021-01-16 19:59:41 +0200

    Update config.js

--- Backend/config.js
+++ Backend/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    SECRET: "Secret for by cript",
+    SECRET: "Secret for bycript",
     DATABASE: "database connection string",
     PORT: "specify the port you want the app to run on development"
 }
